551da354d60730e0d70a323e320af1a0d5cfa4a8
Lakin <lakin.awel@aastustudent.edu.et>
2026-05-21 10:36:40 +0300

    Update Event.h

--- Event.h
+++ Event.h
@@ -4,56 +4,46 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <iomanip>
+#include <algorithm>
 #include "Types.h"
 
 using namespace std;
 
 struct Event {
-    string id, title, description, venueId;
-    EventType type;
-    EventStatus status;
-    Date start, end;
-    int maxCapacity, currentRegs;
-    Budget budget;
-    vector<RSVP> rsvpList;
+    string id, title;
+    int maxCapacity;
+    vector<RSVP> rsvpList; // STL vector: manages memory for you!
 
     Event(string i = "", string t = "") 
-        : id(i), title(t), type(OTHER), status(DRAFT), 
-          maxCapacity(100), currentRegs(0) {}
+        : id(i), title(t), maxCapacity(100) {}
 
-    // --- Logic Functions ---
+    // --- Logic Functions using STL ---
 
     bool registerUser(string userId, string userName) {
-        if (currentRegs < maxCapacity) {
+        if (rsvpList.size() < (size_t)maxCapacity) {
             RSVP r(userId, id, userName);
             r.status = CONFIRMED;
-            rsvpList.push_back(r);
-            currentRegs++;
+            rsvpList.push_back(r); // STL: adds to the end automatically
             return true;
         }
         return false;
     }
 
     bool cancelRegistration(string userId) {
-        for (size_t i = 0; i < rsvpList.size(); i++) {
-            if (rsvpList[i].userId == userId) {
-                rsvpList.erase(rsvpList.begin() + i);
-                currentRegs--;
-                return true;
-            }
+        // STL: find the item and remove it cleanly
+        auto it = remove_if(rsvpList.begin(), rsvpList.end(), 
+                  [&](const RSVP& r) { return r.userId == userId; });
+        
+        if (it != rsvpList.end()) {
+            rsvpList.erase(it, rsvpList.end());
+            return true;
         }
         return false;
     }
 
-    void display() const {
-        cout << "\n--- Event: " << title << " ---\n"
-             << "Status: " << status << " | Capacity: " << currentRegs << "/" << maxCapacity << endl;
-    }
-
     void displayAttendees() const {
         cout << "\nAttendees for " << title << ":\n";
-        for (const auto& r : rsvpList) {
+        for (const auto& r : rsvpList) { // STL: range-based for loop
             cout << "- " << r.userName << " (ID: " << r.userId << ")\n";
         }
     }
